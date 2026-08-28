9eccf53b93ff04db6ce60547340002ac83cc804c
eepyemy <emy.ict@proton.me>
2026-08-28 16:04:36 +0200

    solving pip3 leaks

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ PIP_REQUIREMENTS="requirements.txt"
 
 
 # logic of installing the pip3 dependencies
+# [run from mac bash conda activate amazing and then which pip3 command ]
 source "${HOME}/conda/etc/profile.d/conda.sh"
-conda activate amazing 
+conda activate amazing
 pip3 install -r $PIP_REQUIREMENTS
