fb508800e3c6bfbc29f12425b8de2bf005bdf766
Maud Barlag <maudbarlag@mac.home>
2026-09-19 17:23:10 +0200

    Fixed printing the rows and entry/exit

--- submodules/display.py
+++ submodules/display.py
@@ -1,23 +1,44 @@
 from pydantic import BaseModel
 
+
 class MazePrinter(BaseModel):
-    """ Parses and displays hexadecimal file as readable maze in terminal.
+    """Parses and displays hexadecimal file as readable maze in terminal.
 
     Attributes:
         width (int): Number of cells per row.
         height (int): Number of rows.
+        entry (str): Coordinates of entry point.
+        exit (str): Coordinates of exit point.
         output_file (str): Path to the hexadecimal file.
     """
 
     width: int
     height: int
+    entry: str
+    exit: str
     output_file: str
+
+    @staticmethod
+    def _parse_coords(coord: str) -> tuple[int, int]:
+        """Converts coordinate string into (x, y) tuple.
+
+        Args:
+            coord (str): _description_
+
+        Returns:
+            tuple[int, int]: _description_
+        """
+        xy = coord.split(",")
+        x = int(xy[0])
+        y = int(xy[1])
+        return x, y
 
     def _parse_hex(self) -> list[list[int]]:
         """Reads maze lines from hexadecimal file to a list of list[int].
 
         Returns:
-            list[list[int]]: Holds the grid in ints of 0 until 15.
+            list[list[int]]: Holds maze cells and entry/exit points
+            in ints of 0 until 17.
         """
         hex_maze: list[list[int]] = []
         with open(self.output_file, "r") as file_obj:
@@ -26,34 +47,71 @@ class MazePrinter(BaseModel):
                 if not line:
                     break
                 hex_maze.append([int(char, 16) for char in line])
+
+        self._mark_point(16, hex_maze)
+        self._mark_point(17, hex_maze)
         return hex_maze
 
-    def display_row(self, hex_row: list[int]) -> None:
-        for top_type in hex_row:
-            if top_type in (0):
-                print("   ", end="")
-            if top_type in (1):
-                print("---", end="")
+    def _mark_point(self, point_type: int, hex_maze: list[list[int]]) -> None:
+        coord: tuple
+        if point_type == 16:
+            coord = self._parse_coords(self.entry)
+        elif point_type == 17:
+            coord = self._parse_coords(self.exit)
+        else:
+            raise ValueError
 
-    
-            if top_type in (1, 5):
-                print("─--", end="")
-            elif top_type == 3:
-                print("--┐", end="")
-            elif top_type in (7, 9, 11, 13, 15):
-                print("┌--", end="")
-            else:
-                print(" x ", end="")
-        print()
+        x, y = coord
+        hex_maze[y].insert(x + 1, point_type)   # This still needs a fix, because by printing A and B, it moves the row 1 character.
+        # hex_maze[y][x] = point_type
+
+    def _display_row(self, hex_row: list[int]) -> None:
+        """Prints the North and West walls of a maze row.
+        Closes the row with a + and │ character.
+
+        Args:
+            hex_row (list[int]): One line of hexadecimal characters.
+        """
+        for top_type in hex_row:
+            if top_type in (0, 2, 4, 6):
+                print("   ", end="")
+            elif top_type in (1, 3, 5, 7):
+                print("---", end="")
+            elif top_type in (8, 10, 12, 14):
+                print("+  ", end="")
+            elif top_type in (9, 11, 13, 15):
+                print("+--", end="")
+        print("+")
 
         for cell_type in hex_row:
-            if cell_type in (0, 1, 4, 5):
+            if cell_type in (0, 1, 2, 3, 4, 5, 6, 7):
                 print("   ", end="")
-            elif cell_type in (2, 3, 6, 7):
-                print("  │", end="")
-            elif cell_type in (8, 9, 12, 13):
+            elif cell_type in (8, 9, 10, 11, 12, 13, 14, 15):
                 print("│  ", end="")
-            elif cell_type in (10, 11, 14, 15):
-                print("│ │", end="")
-        print()
+            elif cell_type == 16:
+                print("A", end="")
+            elif cell_type == 17:
+                print("B", end="")
+        print("│")
 
+    def _display_bottom(self, hex_maze: list[list[int]]) -> None:   # Used a different parsing technique: comparing at bit level. More readable?
+        """Prints the closing South walls of the maze.
+
+        Args:
+            hex_maze (list[list[int]]): Used to find last row.
+        """
+        west = 8
+        bottom_row: list[int] = hex_maze[self.height - 1]
+
+        for bottom_type in bottom_row:
+            if bottom_type & west:
+                print("+--", end="")
+            else:
+                print("---", end="")
+        print("+")
+
+    def display_maze(self) -> None:
+        ascii_maze = self._parse_hex()
+        for row in ascii_maze:
+            self._display_row(row)
+        self._display_bottom(ascii_maze)
--- tests/display_test.py
+++ tests/display_test.py
@@ -3,10 +3,7 @@ from submodules.display import MazePrinter
 # Run from project root as a module: python3 -m tests.display_test
 
 if __name__ == '__main__':
-    config = {"height": "5", "width": "5", "output_file": "hex_maze.txt"}
+    config = {"width": "25", "height": "20", "entry": "0,0", "exit": "19,14",
+              "output_file": "hex_maze.txt"}
     printer = MazePrinter(**config)
-    hex_maze = printer._parse_hex()
-    print(hex_maze)
-
-    for row in hex_maze:
-        printer.display_row(row)
+    printer.display_maze()
